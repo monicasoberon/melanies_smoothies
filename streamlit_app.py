bc60b2aebc8896a7761c2cd42e502cc4846511a8
Mónica Soberón Zubía <monicasoberon2747@gmail.com>
2024-08-23 08:21:57 -0600

    Update streamlit_app.py
    indentation fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-    import requests
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
